e851c55b7e0b0b8257844c18d69f59c37d8386b0
Hoa <hoanguyen02122002@gmail.com>
2024-07-24 22:49:54 +0700

    Xoa theo điều kiện

--- src/javabasic/learn_collection/VidDuArrayList.java
+++ src/javabasic/learn_collection/VidDuArrayList.java
@@ -1,6 +1,7 @@
 package javabasic.learn_collection;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class VidDuArrayList {
     public static void main(String[] args) {
@@ -13,8 +14,17 @@ public class VidDuArrayList {
         arrayList1.add("Playwright");
         arrayList1.add("Cypress");
         arrayList1.add("Selenium");
+        arrayList1.add("Katalon");
+        arrayList1.add("Selenium");
+        arrayList1.add("AkaAT Studio");
         arrayList1.add("Selenium");
         arrayList1.add("Selenium");
+        arrayList1.add("Playwright");
+        arrayList1.add("Cypress");
+        arrayList1.add("Selenium");
+        arrayList1.add("Katalon");
+        arrayList1.add("Selenium");
+        arrayList1.add("AkaAT Studio");
         arrayList1.add("Selenium");
 
         arrayList1.add(1, "Katalon");
@@ -22,41 +32,57 @@ public class VidDuArrayList {
 
         //Duyệt phần tử trong arraylist
         //Dùng for cơ bản
-        for (int i = 0; i < arrayList1.size(); i++) {
-            System.out.println(arrayList1.get(i));
-        }
-
-        //Dùng for cải tiến
-        for (String i : arrayList1) {
-            System.out.println(i);
-        }
-
-        //Khai báo ArrayList thứ 2
-        ArrayList<String> arrayList2 = new ArrayList<>();
-        arrayList2.add("Java");
-        arrayList2.add("Python");
-
-        //add All ArrayList2 to ArrayList1
-        arrayList1.addAll(arrayList2);
-
-        System.out.println("ArrayList1 sau khi add all ArrayList2: ");
-        for (String i : arrayList1) {
-            System.out.println(i);
-        }
-
-        //Remove 1 phần tử trong ArrayList
-        arrayList1.remove("Selenium");
-        arrayList1.remove(2);
-
-
-        System.out.println("ArrayList1 sau khi xóa: ");
-        for (String i : arrayList1) {
-            System.out.println(i);
-        }
+//        for (int i = 0; i < arrayList1.size(); i++) {
+//            System.out.println(arrayList1.get(i));
+//        }
+//
+//        //Dùng for cải tiến
+//        for (String i : arrayList1) {
+//            System.out.println(i);
+//        }
+//
+//        //Khai báo ArrayList thứ 2
+//        ArrayList<String> arrayList2 = new ArrayList<>();
+//        arrayList2.add("Java");
+//        arrayList2.add("Python");
+//
+//        //add All ArrayList2 to ArrayList1
+//        arrayList1.addAll(arrayList2);
+//
+//        System.out.println("ArrayList1 sau khi add all ArrayList2: ");
+//        for (String i : arrayList1) {
+//            System.out.println(i);
+//        }
+//
+//        //Remove 1 phần tử trong ArrayList
+//        arrayList1.remove("Selenium");
+//        arrayList1.remove(2);
+//
+//
+//        System.out.println("ArrayList1 sau khi xóa: ");
+//        for (String i : arrayList1) {
+//            System.out.println(i);
+//        }
         //remove có điều kiện
         int size = arrayList1.size();
         System.out.println(size);
+
+        for (int i = 0; i < size; i++) {
+            if (arrayList1.get(i).equals("Selenium")) {
+                arrayList1.remove(i);
+                size = arrayList1.size();
+                i--;
+                System.out.println("Đã xóa phần tử tại vị trí " + i);
+                System.out.println("Mảng còn lại: " + arrayList1);
+                System.out.println("Độ dài mảng còn lại: " + arrayList1.size());
+            }
+        }
+//
+
         for (int i = size - 1; i >= 0; i--) {
+
+
+
             if (arrayList1.get(i).equals("Selenium")) {
                 arrayList1.remove(i);
 
@@ -65,6 +91,16 @@ public class VidDuArrayList {
                 System.out.println("Độ dài mảng còn lại: " + arrayList1.size());
             }
         }
+
+        //
+        Iterator<String> iterator = arrayList1.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().equalsIgnoreCase("Selenium")) {
+                iterator.remove();
+            }
+        }
+
+
         System.out.println("ArrayList sau khi xóa: ");
         for (String i : arrayList1) {
             System.out.println(i);
